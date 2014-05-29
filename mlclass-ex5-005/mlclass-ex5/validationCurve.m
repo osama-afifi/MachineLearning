965188819804a1d906176d2cefe916b960272db5
osama-afifi <osama.egt@gmail.com>
2014-05-29 15:01:21 +0200

    Finding Optimal Lambda for Regularization

--- mlclass-ex5-005/mlclass-ex5/validationCurve.m
+++ mlclass-ex5-005/mlclass-ex5/validationCurve.m
@@ -25,26 +25,14 @@ error_val = zeros(length(lambda_vec), 1);
 %               you the errors obtained after training with 
 %               lambda = lambda_vec(i)
 %
-% Note: You can loop over lambda_vec with the following:
-%
-%       for i = 1:length(lambda_vec)
-%           lambda = lambda_vec(i);
-%           % Compute train / val errors when training linear 
-%           % regression with regularization parameter lambda
-%           % You should store the result in error_train(i)
-%           % and error_val(i)
-%           ....
-%           
-%       end
-%
-%
 
-
-
-
-
-
-
+r = length(lambda_vec);
+for i = 1:r;
+lambda = lambda_vec(i);
+[theta] = trainLinearReg(X, y, lambda);
+error_train(i) = linearRegCostFunction(X, y, theta, 0); % Note No Regularization in Error Calculation
+error_val(i) = linearRegCostFunction(Xval, yval, theta, 0); % Note No Regularization in Error Calculation
+endfor
 
 
 
