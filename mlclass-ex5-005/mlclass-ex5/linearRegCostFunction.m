045357d3997135e61cfab63b8e451d2221fd5f55
osama-afifi <osama.egt@gmail.com>
2014-05-29 13:59:27 +0200

    Added Regularized Linear Regression Cost Function and Gradient

--- mlclass-ex5-005/mlclass-ex5/linearRegCostFunction.m
+++ mlclass-ex5-005/mlclass-ex5/linearRegCostFunction.m
@@ -19,16 +19,18 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+%Note X : m x n
 
+%Cost
+hypo = X * theta; % m x 1
+SQDiff = (hypo - y) .^2; % m x 1
+J = 1.0 / (2.0 * m) * sum(SQDiff); 
+grad = (1.0/m)*((hypo - y)' * X)'; % n x 1
 
-
-
-
-
-
-
-
-
+%Regularization
+modifiedTheta = [0 ; theta(2:end)];
+J = J + ( (lambda / (2*m) ) * (modifiedTheta' * modifiedTheta));
+grad = grad + ((lambda/m) * modifiedTheta);
 
 % =========================================================================
 
